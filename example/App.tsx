689dfb8fb143520a9891286af84fbce019f14a49
Aleksander Atamanczuk <aleksander.atamanczuk@gmail.com>
2026-08-06 21:49:59 +0200

    chore: simplify example app and configure Metro for monorepo development

--- example/App.tsx
+++ example/App.tsx
@@ -1,37 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { BUNNY_STREAM_REACT_NATIVE_VERSION } from 'bunny-stream-react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>
+        Bunny Stream React Native{'\n'}v{BUNNY_STREAM_REACT_NATIVE_VERSION}
+      </Text>
     </View>
   );
 }
@@ -39,6 +14,13 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
